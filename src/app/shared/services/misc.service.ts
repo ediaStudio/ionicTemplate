b4525bef99022df4b13b7f988b8f899cc6b777fd
wahid <sawyer179@yahoo.fr>
2025-05-20 01:30:49 +0700

    add spinner and toast

--- src/app/shared/services/misc.service.ts
+++ src/app/shared/services/misc.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {AlertController} from "@ionic/angular";
+import {AlertController, LoadingController, ToastController} from "@ionic/angular";
 import {TranslateService} from "@ngx-translate/core";
 import {ELang, LANGUAGES} from "@models/language";
 
@@ -8,8 +8,62 @@ import {ELang, LANGUAGES} from "@models/language";
 })
 export class MiscService {
 
-    constructor(private alertController: AlertController,
+    loading: any;
+    toast: any;
+
+    constructor(private loadingCtrl: LoadingController,
+                private alertController: AlertController,
+                private toastController: ToastController,
                 private translate: TranslateService) {
+    }
+
+    async showLoading(message: string = 'pleaseWait') {
+        this.dismissLoading();
+        this.loading = await this.loadingCtrl.create({
+            spinner: "bubbles",
+            duration: 30000,
+            mode: "ios",
+            message: this.translate.instant(message),
+            translucent: false,
+            showBackdrop: true,
+            cssClass: ""
+        });
+        return await this.loading.present();
+    }
+
+    dismissLoading() {
+        if (this.loading) {
+            this.loading.dismiss();
+        }
+    }
+
+    async presentToastWithOptions(header: any, isError = false) {
+        if (this.toast) {
+            this.toast.dismiss();
+        }
+        header = header?.message || header;
+        header = header ? header : isError ? 'error' : 'unknown';
+        this.toast = await this.toastController.create({
+            header: this.translate.instant(header),
+            message: "",
+            position: "middle",
+            animated: true,
+            mode: "ios",
+            cssClass: "",
+            color: isError ? "danger" : "primary",
+            duration: 1800,
+            keyboardClose: false,
+            translucent: false
+        });
+        this.toast.present();
+    }
+
+    displayError(error?: any) {
+        const message = error?.message || JSON.stringify(error) || 'error';
+        if (!error) {
+            return;
+        }
+        this.presentToastWithOptions(message, true);
     }
 
     async presentAlertLanguage(): Promise<ELang> {
